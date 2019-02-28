ba1e9fb6cc7daa0ca4dbf024e1acba43d0353c4c
Alexander Kurta <41297214+AlexKurta@users.noreply.github.com>
2019-02-28 18:32:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Catalog from './components/Catalog'
-import { Container } from 'react-bootstrap'
 
 class App extends React.Component {
   getCatalog = ({ location }) => {
-    return (
-      <Container className="row">
-        <Catalog url={location.pathname} />
-      </Container>
-    )
+    return <Catalog url={location.pathname} />
   }
 
   render() {
